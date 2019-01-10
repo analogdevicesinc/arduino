75dbaf22baae515e06c11748d3b3c9095c3fe7f1
Antoniu Miclaus <antoniu.miclaus@analog.com>
2019-01-10 16:42:55 +0200

    Add Travis to arduino repo.
    The purpose of it is to perform static analysis with the cppcheck tool
    on all source files and and check if code style of the files
    modified/added per commit correspond to the standards.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

---
+++ ci/travis/run_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+. ./ci/travis/lib.sh
+
+build_default() {
+    . ./build/cppcheck.sh
+}
+
+build_astyle() {
+    export ASTYLE_EXT_LIST=".cpp .hpp .ino"
+    . ./build/astyle.sh
+}
+
+build_${BUILD_TYPE:-default}
