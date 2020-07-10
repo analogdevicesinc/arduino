analogdevicesinc/arduino

commit c038c16c8e0c3e1cf45c4a84efe531b1cd92df68

Arduino shields examples: ADXL362 ADXL372 CN0391 CN0396 CN0397
First revision.

Signed-off-by: Mircea Caprioru <mircea.caprioru@analog.com>

Arduino Uno R3/examples/ADXL372_example/adxl372.cpp
@@ -1,0 +1,335 @@
+/***************************************************************************//**
+ *   @file   adxl372.c
+ *   @brief  ADXL372 device driver.
+********************************************************************************
+ * Copyright 2017(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+*******************************************************************************/
+#include "adxl372.h"
+#include "Communication.h"
+
+//#define ADXL_DEBUG
+
+static int adxl_read_reg_multiple(adxl_spi_handle *spi, unsigned char reg,
+                                  unsigned char count, unsigned char *val)
+{
+    reg = reg << 1 | ADXL_SPI_RNW;
+    return spi_write_then_read(spi, &reg, 1, val, count);   
+}
+
+int adxl_read_reg(adxl_spi_handle *spi,
+                         unsigned char reg, unsigned char *val)
+{
+    return adxl_read_reg_multiple(spi, reg, 1, val);
+}
+
+int adxl_write_reg(adxl_spi_handle *spi, unsigned char reg,
+                          unsigned char val)
+{
+    unsigned char tmp[2];
+    tmp[0] = reg << 1;
+    tmp[1] = val;
+
+#ifdef ADXL_DEBUG
+    spi_write_then_read(spi, tmp, 2, NULL, 0);
+        adxl_delay(1);
+    adxl_read_reg(spi, reg, tmp);
+    if (val != tmp[0])
+      dnm_ucli_printf("verify failed REG 0x%X - 0x%X != 0x%X\r\n", reg, val, tmp[0]);
+    
+    return 0;
+    
+#else
+    return spi_write_then_read(spi, tmp, 2, NULL, 0); 
+#endif    
+}
+
+static int adxl_update_reg(adxl_spi_handle *spi, unsigned char reg,
+                           unsigned char mask, unsigned char shift, unsigned char val)
+{
+    unsigned char tmp;
+    int err;
+
+    err = adxl_read_reg(spi, reg, &tmp);
+    if (err < 0)
+        return err;
+
+    tmp &= mask;
+    tmp |= (val << shift) & ~mask;
+
+    return adxl_write_reg(spi, reg, tmp);
+}
+
+int adxl372_Set_Op_mode(struct adxl372_device *dev, ADXL372_OP_MODE mode)
+{
+    return adxl_update_reg(dev->spi, ADI_ADXL372_POWER_CTL, PWRCTRL_OPMODE_MASK, 0,
+                           mode);
+}
+
+int adxl372_Set_ODR(struct adxl372_device *dev, ADXL372_ODR odr)
+{
+    return adxl_update_reg(dev->spi, ADI_ADXL372_TIMING, TIMING_ODR_MASK,
+                           TIMING_ODR_POS, odr);
+}
+
+int adxl372_Set_WakeUp_Rate(struct adxl372_device *dev, ADXL372_WUR wur)
+{
+    return adxl_update_reg(dev->spi, ADI_ADXL372_TIMING, TIMING_WUR_MASK,
+                           TIMING_WUR_POS, wur);
+}
+
+int adxl372_Set_BandWidth(struct adxl372_device *dev, ADXL372_BW bw)
+{
+    return adxl_update_reg(dev->spi, ADI_ADXL372_MEASURE, MEASURE_BANDWIDTH_MASK, 0,
+                           bw);
+}
+
+int adxl372_Set_Autosleep(struct adxl372_device *dev, bool enable)
+{
+    return adxl_update_reg(dev->spi, ADI_ADXL372_MEASURE, MEASURE_AUTOSLEEP_MASK,
+                           MEASURE_AUTOSLEEP_POS, enable);
+}
+
+int adxl372_Set_Act_Proc_Mode(struct adxl372_device *dev,
+                              ADXL372_ACT_PROC_MODE mode)
+{
+    return adxl_update_reg(dev->spi, ADI_ADXL372_MEASURE, MEASURE_ACTPROC_MASK,
+                           MEASURE_ACTPROC_POS, mode);
+}
+
+int adxl372_Set_InstaOn_Thresh(struct adxl372_device *dev,
+                               ADXL_INSTAON_THRESH mode)
+{
+    return adxl_update_reg(dev->spi, ADI_ADXL372_POWER_CTL,
+                           PWRCTRL_INSTON_THRESH_MASK, INSTAON_THRESH_POS, mode);
+}
+
+int adxl372_Set_Activity_Threshold(struct adxl372_device *dev,
+                                   unsigned short thresh, bool referenced, bool enable)
+{
+    int err = adxl372_Set_Op_mode(dev, STAND_BY); /* FIXME ? */
+    if (err < 0)
+        return err;
+
+    adxl_write_reg(dev->spi, ADI_ADXL372_X_THRESH_ACT_H, thresh >> 3);
+    adxl_write_reg(dev->spi, ADI_ADXL372_X_THRESH_ACT_L,
+                   (thresh << 5) | (referenced << 1) | enable);
+    adxl_write_reg(dev->spi, ADI_ADXL372_Y_THRESH_ACT_H, thresh >> 3);
+    adxl_write_reg(dev->spi, ADI_ADXL372_Y_THRESH_ACT_L, (thresh << 5) | enable);
+    adxl_write_reg(dev->spi, ADI_ADXL372_Z_THRESH_ACT_H, thresh >> 3);
+    adxl_write_reg(dev->spi, ADI_ADXL372_Z_THRESH_ACT_L, (thresh << 5) | enable);
+
+    return err;
+}
+
+int adxl372_Set_Activity2_Threshold(struct adxl372_device *dev,
+                                   unsigned short thresh, bool referenced, bool enable)
+{
+    int err = adxl372_Set_Op_mode(dev, STAND_BY); /* FIXME ? */
+    if (err < 0)
+        return err;
+
+    adxl_write_reg(dev->spi, ADI_ADXL372_X_THRESH_ACT2_H, thresh >> 3);
+    adxl_write_reg(dev->spi, ADI_ADXL372_X_THRESH_ACT2_L,
+                   (thresh << 5) | (referenced << 1) | enable);
+    adxl_write_reg(dev->spi, ADI_ADXL372_Y_THRESH_ACT2_H, thresh >> 3);
+    adxl_write_reg(dev->spi, ADI_ADXL372_Y_THRESH_ACT2_L, (thresh << 5) | enable);
+    adxl_write_reg(dev->spi, ADI_ADXL372_Z_THRESH_ACT2_H, thresh >> 3);
+    adxl_write_reg(dev->spi, ADI_ADXL372_Z_THRESH_ACT2_L, (thresh << 5) | enable);
+
+    return err;
+}
+
+int adxl372_Set_Inactivity_Threshold(struct adxl372_device *dev,
+                                     unsigned short thresh, bool referenced, bool enable)
+{
+    int err = adxl372_Set_Op_mode(dev, STAND_BY);  /* FIXME ? */
+    if (err < 0)
+        return err;
+
+    adxl_write_reg(dev->spi, ADI_ADXL372_X_THRESH_INACT_H, thresh >> 3);
+    adxl_write_reg(dev->spi, ADI_ADXL372_X_THRESH_INACT_L,
+                   (thresh << 5) | (referenced << 1) | enable);
+    adxl_write_reg(dev->spi, ADI_ADXL372_Y_THRESH_INACT_H, thresh >> 3);
+    adxl_write_reg(dev->spi, ADI_ADXL372_Y_THRESH_INACT_L, (thresh << 5) | enable);
+    adxl_write_reg(dev->spi, ADI_ADXL372_Z_THRESH_INACT_H, thresh >> 3);
+    adxl_write_reg(dev->spi, ADI_ADXL372_Z_THRESH_INACT_L, (thresh << 5) | enable);
+
+    return err;
+}
+
+int adxl372_Set_Activity_Time(struct adxl372_device *dev, unsigned char time)
+{
+    return adxl_write_reg(dev->spi, ADI_ADXL372_TIME_ACT, time);
+}
+
+int adxl372_Set_Inactivity_Time(struct adxl372_device *dev, unsigned short time)
+{
+    adxl_write_reg(dev->spi, ADI_ADXL372_TIME_INACT_H, time >> 8);
+
+    return adxl_write_reg(dev->spi, ADI_ADXL372_TIME_INACT_L, time & 0xFF);
+}
+
+int adxl372_Set_Filter_Settle(struct adxl372_device *dev,
+                              ADXL372_Filter_Settle mode)
+{
+    return adxl_update_reg(dev->spi, ADI_ADXL372_POWER_CTL,
+                           PWRCTRL_FILTER_SETTLE_MASK, PWRCTRL_FILTER_SETTLE_POS, mode);
+}
+
+int adxl372_Get_DevID(struct adxl372_device *dev, unsigned char *DevID)
+{
+    return adxl_read_reg_multiple(dev->spi, ADI_ADXL372_ADI_DEVID, 1, DevID);
+}
+
+int adxl372_Get_Status_Register(struct adxl372_device *dev,
+                                unsigned char *adxl_status)
+{
+    return adxl_read_reg(dev->spi, ADI_ADXL372_STATUS_1, adxl_status);
+}
+
+int adxl372_Get_ActivityStatus_Register(struct adxl372_device *dev,
+                                        unsigned char *adxl_status)
+{
+    return adxl_read_reg(dev->spi, ADI_ADXL372_STATUS_2, adxl_status);
+}
+
+#define SWAP16(x) ((x) = (((x) & 0x00FF) << 8) | (((x) & 0xFF00) >> 8))
+#define SHIFT4(x) ((x) = (x) >> 4)
+
+int adxl372_Get_Highest_Peak_Accel_data(struct adxl372_device *dev,
+                                        AccelTriplet_t *max_peak)
+{
+    int err = adxl_read_reg_multiple(dev->spi, ADI_ADXL372_X_MAXPEAK_H, 6,
+                                     (unsigned char *) max_peak);
+
+#ifdef L_ENDIAN
+    SWAP16(max_peak->x);
+    SWAP16(max_peak->y);
+    SWAP16(max_peak->z);
+#endif
+
+    SHIFT4(max_peak->x);
+    SHIFT4(max_peak->y);
+    SHIFT4(max_peak->z);
+
+    return err;
+}
+
+int adxl372_Get_Accel_data(struct adxl372_device *dev,
+                           AccelTriplet_t *accel_data)
+{
+    unsigned char status;
+    int err;
+    
+    do {
+        adxl372_Get_Status_Register(dev, &status);
+    } while (!(status & DATA_RDY));
+
+    err = adxl_read_reg_multiple(dev->spi, ADI_ADXL372_X_DATA_H, 6,
+                                 (unsigned char *) accel_data);
+
+#ifdef L_ENDIAN
+  
+    SWAP16(accel_data->x);
+    SWAP16(accel_data->y);
+    SWAP16(accel_data->z);
+#endif
+
+    SHIFT4(accel_data->x);
+    SHIFT4(accel_data->y);
+    SHIFT4(accel_data->z);    
+
+    return err;
+}
+
+int adxl372_Reset(struct adxl372_device *dev)
+{
+    int err = adxl_write_reg(dev->spi, ADI_ADXL372_SRESET, 0x52);
+
+    delay(1);
+
+    return err;
+}
+
+int adxl372_Configure_FIFO(struct adxl372_device *dev,
+                           unsigned short fifo_samples,
+                           ADXL372_FIFO_MODE fifo_mode,
+                           ADXL372_FIFO_FORMAT fifo_format)
+{
+    unsigned char config;
+    int err;
+
+    adxl372_Set_Op_mode(dev, STAND_BY);
+
+    if (fifo_samples > 512)
+        return -1;
+
+    fifo_samples -= 1;
+
+    config = ((unsigned char)fifo_mode << FIFO_CRL_MODE_POS) |
+             ((unsigned char)fifo_format << FIFO_CRL_FORMAT_POS) |
+             ((fifo_samples > 0xFF) << FIFO_CRL_SAMP8_POS);
+
+    err = adxl_write_reg(dev->spi, ADI_ADXL372_FIFO_SAMPLES, fifo_samples & 0xFF);
+    if (err < 0)
+        return err;
+
+    err = adxl_write_reg(dev->spi, ADI_ADXL372_FIFO_CTL, config);
+    if (err < 0)
+        return err;
+
+    dev->fifo_config.samples = fifo_samples + 1;
+    dev->fifo_config.mode = fifo_mode;
+    dev->fifo_config.format = fifo_format;
+
+    return err;
+}
+
+int adxl372_Get_FIFO_data(struct adxl372_device *dev, short *samples)
+{
+    int err, i;
+    if(dev->fifo_config.mode == BYPASSED)
+        return -1;
+
+    err = adxl_read_reg_multiple(dev->spi, ADI_ADXL372_FIFO_DATA,
+                                 dev->fifo_config.samples * 2, (unsigned char *) samples);
+
+#ifdef L_ENDIAN
+    for (i = 0; i < dev->fifo_config.samples; i++)
+        SWAP16(samples[i]);
+#endif
+
+    return err;
+}

Arduino Uno R3/examples/ADXL362_example/ADXL362.cpp
@@ -1,0 +1,168 @@
+/**
+******************************************************************************
+*   @file     ADXL362.c
+*   @brief    Source file for ADXL362 accelerometer control.
+*   @version  V0.3
+*   @author   ADI
+*   @date     March 2016
+*  @par Revision History:
+*  - V0.1, September 2015: initial version.
+*  - V0.2, October 2015: removed ACC definitions, added configuration for CSACC pin and revision history.
+*  - V0.3, March 2016: added support for ADXL362 INT pin.
+*
+*******************************************************************************
+* Copyright 2015(c) Analog Devices, Inc.
+*
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*  - Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+*  - Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in
+*    the documentation and/or other materials provided with the
+*    distribution.
+*  - Neither the name of Analog Devices, Inc. nor the names of its
+*    contributors may be used to endorse or promote products derived
+*    from this software without specific prior written permission.
+*  - The use of this software may or may not infringe the patent rights
+*    of one or more patent holders.  This license does not release you
+*    from the requirement that you obtain separate licenses from these
+*    patent holders to use this software.
+*  - Use of the software either in source or binary form, must be run
+*    on or directly connected to an Analog Devices Inc. component.
+*
+* THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY
+* AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*******************************************************************************
+**/
+
+/***************************** Include Files **********************************/
+#include <stdio.h>
+
+#include "ADXL362.h"
+#include "Communication.h"
+
+/****************************** Global Data ***********************************/
+
+int16_t i16SensorX;
+int16_t i16SensorY;
+int16_t i16SensorZ;
+int16_t i16SensorT;
+
+volatile uint32_t ui32timer_counter = 0;
+
+
+/*************************** Module static data *******************************/
+
+/* Accelerometer scan counter */
+static uint32_t ui32ScanSensorCounter;
+
+/************************* Global scope functions *****************************/
+
+/**
+   @brief Initialization the accelerometer sensor
+
+   @return none
+
+**/
+void Sensor_Init(void)
+{          
+   pinMode(CSACC_PIN, OUTPUT); /* Set CSACC pin as output */
+
+   pinMode(INTACC_PIN, INPUT); /* Set INTACC pin as input */
+
+   SPI_Write(SOFT_RESET_REG, 0x52, SPI_WRITE_REG);  /* Soft reset accelerometer */
+
+   delay(100);                         /* Wait at least 0.5 ms */
+
+   /* Set activity threshold */
+   SPI_Write(THRESH_ACT_L, ACT_VALUE & 0xFF, SPI_WRITE_REG);
+   SPI_Write(THRESH_ACT_H, ACT_VALUE >> 8, SPI_WRITE_REG);
+
+   SPI_Write(TIME_ACT, (ACT_TIMER / 10), SPI_WRITE_REG);          /* Set activity time at 100Hz data rate */
+
+   /* Set inactivity threshold */
+   SPI_Write(THRESH_INACT_L, INACT_VALUE & 0xFF, SPI_WRITE_REG);
+   SPI_Write(THRESH_INACT_H, INACT_VALUE >> 8, SPI_WRITE_REG);
+
+   /* Set inactivity time at 100Hz data rate */
+   SPI_Write(TIME_INACT_L, ((INACT_TIMER * 100) & 0xFF), SPI_WRITE_REG);
+   SPI_Write(TIME_INACT_H, ((INACT_TIMER * 100) >> 8), SPI_WRITE_REG);
+
+   SPI_Write(ACT_INACT_CTL, 0x3F, SPI_WRITE_REG);         /* Set Loop mode, referenced mode for activity and inactivity, enable activity and inactivity functionality */
+
+#if(ADXL_INT_SEL == INTACC_PIN_1)
+   SPI_Write(INTMAP1, 0x40, SPI_WRITE_REG);                  /* Map the awake status to INT1 pin */
+#elif(ADXL_INT_SEL == INTACC_PIN_2)
+   SPI_Write(INTMAP2, 0x40, SPI_WRITE_REG);                  /* Map the awake status to INT2 pin */
+#endif
+}
+
+/**
+   @brief Turns on accelerometer measurement mode.
+
+   @return none
+
+**/
+void Sensor_Start(void)
+{
+   uint8_t ui8temp;
+
+   ui8temp = (uint8_t)SPI_Read(POWER_CTL_REG, SPI_READ_ONE_REG);       /* Read POWER_CTL register, before modifying it */
+
+   ui8temp = ui8temp | 0x02;                                          /* Set measurement bit in POWER_CTL register */
+
+   SPI_Write(POWER_CTL_REG, ui8temp, SPI_WRITE_REG);                    /* Write the new value to POWER_CTL register */
+}
+
+
+/**
+   @brief Puts the accelerometer into standby mode.
+
+   @return none
+
+**/
+void Sensor_Stop(void)
+{
+   uint8_t ui8temp;
+
+   ui8temp = (uint8_t)SPI_Read(POWER_CTL_REG, SPI_READ_ONE_REG);        /*Read POWER_CTL register, before modifying it */
+
+   ui8temp = ui8temp & 0xFC;                                      /* Clear measurement bit in POWER_CTL register */
+
+   SPI_Write(POWER_CTL_REG, ui8temp, SPI_WRITE_REG);                 /* Write the new value to POWER_CTL register */
+
+}
+
+/**
+   @brief Reads the accelerometer data.
+
+   @return none
+
+**/
+void Sensor_Scan(void)
+{
+      i16SensorX = SPI_Read(XDATA_L_REG, SPI_READ_TWO_REG);
+
+      i16SensorY = SPI_Read(YDATA_L_REG, SPI_READ_TWO_REG);
+
+      i16SensorZ = SPI_Read(ZDATA_L_REG, SPI_READ_TWO_REG);
+
+      i16SensorT = SPI_Read(TEMP_L_REG, SPI_READ_TWO_REG);
+
+      Serial.print(i16SensorX);  Serial.print("\t"); Serial.print(i16SensorY);  Serial.print("\t"); Serial.println(i16SensorZ);
+
+      delay(200);
+}
+

Arduino Uno R3/examples/ADXL362_example/ADXL362.h
@@ -1,0 +1,123 @@
+/**
+******************************************************************************
+*   @file     ADXL362.h
+*   @brief    Header file for ADXL362 accelerometer control.
+*   @version  V0.3
+*   @author   ADI
+*   @date     March 2016
+*  @par Revision History:
+*  - V0.1, September 2015: initial version.
+*  - V0.2, October 2015: moved ACC definitions and added revision history.
+*  - V0.3, March 2016: updated temperature parameters - name and value.
+*
+*******************************************************************************
+* Copyright 2015(c) Analog Devices, Inc.
+*
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*  - Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+*  - Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in
+*    the documentation and/or other materials provided with the
+*    distribution.
+*  - Neither the name of Analog Devices, Inc. nor the names of its
+*    contributors may be used to endorse or promote products derived
+*    from this software without specific prior written permission.
+*  - The use of this software may or may not infringe the patent rights
+*    of one or more patent holders.  This license does not release you
+*    from the requirement that you obtain separate licenses from these
+*    patent holders to use this software.
+*  - Use of the software either in source or binary form, must be run
+*    on or directly connected to an Analog Devices Inc. component.
+*
+* THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY
+* AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*******************************************************************************
+**/
+#ifndef ADXL362_H_
+#define ADXL362_H_
+
+#include "Arduino.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/********************************* Definitions ********************************/
+
+/* Accelerometer registers addresses */
+#define STATUS_REG         0x0B
+#define XDATA_L_REG        0x0E
+#define YDATA_L_REG        0x10
+#define ZDATA_L_REG        0x12
+#define TEMP_L_REG         0x14
+#define SOFT_RESET_REG     0x1F
+#define THRESH_ACT_L       0x20
+#define THRESH_ACT_H       0x21
+#define TIME_ACT           0x22
+#define THRESH_INACT_L     0x23
+#define THRESH_INACT_H     0x24
+#define TIME_INACT_L       0x25
+#define TIME_INACT_H       0x26
+#define ACT_INACT_CTL      0x27
+#define INTMAP1            0x2A
+#define INTMAP2            0x2B
+#define POWER_CTL_REG      0x2D
+
+
+/**************************** Configuration parameters **********************/
+
+#define TEMP_ADC        0     /* Temperature display units: 1 for ADC, 0 for degrees Celsius */
+
+/* Temperature parameters */
+#define ACC_TEMP_BIAS             (float)350         /* Accelerometer temperature bias(in ADC codes) at 25 Deg C */
+#define ACC_TEMP_SENSITIVITY      (float)0.065       /* Accelerometer temperature sensitivity  from datasheet (DegC per Code) */
+
+#define SCAN_SENSOR_TIME   500    /* Accelerometer scan interval in ms */
+
+#define ACT_VALUE          50     /* Activity threshold value */
+
+#define INACT_VALUE        50     /* Inactivity threshold value */
+
+#define ACT_TIMER          100    /* Activity timer value in ms */
+
+#define INACT_TIMER        10     /* Inactivity timer value in seconds */
+
+
+/****************************** Global Data ***********************************/
+
+extern int16_t i16SensorX;
+extern int16_t i16SensorY;
+extern int16_t i16SensorZ;
+extern int16_t i16SensorT;
+
+extern volatile uint32_t ui32timer_counter;
+
+
+/*************************** Functions prototypes *****************************/
+
+void Sensor_Init(void);
+void Sensor_Start(void);
+void Sensor_Stop(void);
+void Sensor_Scan(void);
+uint8_t Sensor_Delay(uint8_t ui8StartFlag, uint32_t *pu32EndTm, uint32_t ui32Delay);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif /* ADXL362_H_ */
+
+/* End Of File */

Arduino Uno R3/examples/ADXL362_example/ADXL362_example.ino
@@ -1,0 +1,167 @@
+#include "ADXL362.h"
+#include "Lcd.h"
+#include "Communication.h"
+#include <SPI.h>
+
+byte ADXL_CS_PIN = 10;
+byte LCD_CS_PIN = 11;
+
+uint8_t ui8s[22];
+uint8_t ui8xu;
+uint8_t ui8xd;
+uint8_t ui8yu;
+uint8_t ui8yd;
+uint8_t ui8all;
+uint8_t ui8awake;
+
+#if TEMP_ADC == 0
+   float f32temp;
+#endif
+
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  SPI.setDataMode(SPI_MODE3); //CPHA = CPOL = 1    MODE = 3
+  delay(1000);
+
+  /* Initialize LCD */
+   Lcd_Init();
+
+   /* Initialize accelerometer */
+   Sensor_Init();
+
+   /* Start accelerometer measurement mode */
+   Sensor_Start();
+
+}
+
+void loop() {
+if (digitalRead(INTACC_PIN)) {
+         if (ui8awake == 0) {
+            ui8awake = 1;
+
+            /* Set BLLCD pin - turn on LCD backlight */
+            digitalWrite(BLLCD_PIN,HIGH);
+
+            Lcd_DisplayString(0, 60, (int8_t *)"[mG]");
+            Lcd_DisplayString(1, 60, (int8_t *)"[mG]");
+            Lcd_DisplayString(2, 60, (int8_t *)"[mG]");
+
+#if TEMP_ADC == 1
+            Lcd_DisplayString(3, 60, (int8_t *)"[ADC]");
+#else
+            Lcd_DisplayString(3, 60, (int8_t *)"[C]");
+#endif
+            ui8xu = 0;
+            ui8xd = 0;
+            ui8yu = 0;
+            ui8yd = 0;
+            ui8all = 0;
+
+            Lcd_DisplaySymbol(0, UP_X, 8, pui8RecInv8x8);
+            Lcd_DisplaySymbol(1, LEFT_X, 8, pui8RecInv8x8);
+            Lcd_DisplaySymbol(1, RIGHT_X, 8, pui8RecInv8x8);
+            Lcd_DisplaySymbol(2, DOWN_X, 8, pui8RecInv8x8);
+            Lcd_DisplaySymbol(1, CENTER_X, 8, pui8RecInv8x8);
+         }
+
+      } else {
+         if (ui8awake == 1) {
+            ui8awake = 0;
+
+            /* Clear BLLCD pin - turn off LCD backlight */
+            digitalWrite(BLLCD_PIN,LOW);
+
+            /* Clear screen */
+            Lcd_FillPages(0, 4, 0x00);
+         }
+      }
+
+      if (ui8awake == 1) {
+         Sensor_Scan();
+        
+         sprintf((char *)ui8s, "x = % 5d", i16SensorX);
+         Lcd_DisplayString(0, 0, (int8_t *)ui8s);
+
+         sprintf((char *)ui8s, "y = % 5d", i16SensorY);
+         Lcd_DisplayString(1, 0, (int8_t *)ui8s);
+
+         sprintf((char *)ui8s, "z = % 5d", i16SensorZ);
+         Lcd_DisplayString(2, 0, (int8_t *)ui8s);
+
+#if TEMP_ADC == 1
+         sprintf((char *)ui8s, "t = % 5d", i16SensorT);
+         Lcd_DisplayString(3, 0, (int8_t *)ui8s);
+#else
+         f32temp = ((float)i16SensorT + ACC_TEMP_BIAS) / (1 / ACC_TEMP_SENSITIVITY);   // -34.625
+         sprintf((char *)ui8s, "t = % 4.1f", f32temp);
+         Lcd_DisplayString(3, 0, (int8_t *)ui8s);
+#endif
+
+         if (i16SensorY > ACC_LIMIT) {
+            if (ui8xu == 0) {
+               ui8xu = 1;
+               Lcd_DisplaySymbol(0, UP_X, 8, pui8Rec8x8);
+            }
+
+         } else {
+            if (ui8xu == 1) {
+               ui8xu = 0;
+               Lcd_DisplaySymbol(0, UP_X, 8, pui8RecInv8x8);
+            }
+         }
+
+         if (i16SensorY < -ACC_LIMIT) {
+            if (ui8xd == 0) {
+               ui8xd = 1;
+               Lcd_DisplaySymbol(2, DOWN_X, 8, pui8Rec8x8);
+            }
+
+         } else {
+            if (ui8xd == 1) {
+               ui8xd = 0;
+               Lcd_DisplaySymbol(2, DOWN_X, 8, pui8RecInv8x8);
+            }
+         }
+
+         if (i16SensorX > ACC_LIMIT) {
+            if (ui8yu == 0) {
+               ui8yu = 1;
+               Lcd_DisplaySymbol(1, RIGHT_X, 8, pui8Rec8x8);
+            }
+
+         } else {
+            if (ui8yu == 1) {
+               ui8yu = 0;
+               Lcd_DisplaySymbol(1, RIGHT_X, 8, pui8RecInv8x8);
+            }
+         }
+
+         if (i16SensorX < -ACC_LIMIT) {
+            if (ui8yd == 0) {
+               ui8yd = 1;
+               Lcd_DisplaySymbol(1, LEFT_X, 8, pui8Rec8x8);
+            }
+
+         } else {
+            if (ui8yd == 1) {
+               ui8yd = 0;
+               Lcd_DisplaySymbol(1, LEFT_X, 8, pui8RecInv8x8);
+            }
+         }
+
+         if ((ui8xu == 0) && (ui8xd == 0) && (ui8yu == 0) && (ui8yd == 0)) {
+            if (ui8all == 0) {
+               ui8all = 1;
+               Lcd_DisplaySymbol(1, CENTER_X, 8, pui8Rec8x8);
+            }
+
+         } else {
+            if (ui8all == 1) {
+               ui8all = 0;
+               Lcd_DisplaySymbol(1, CENTER_X, 8, pui8RecInv8x8);
+            }
+         }
+      }
+}

commit 7fb937d31bb7f6d5dcf3107b9be1a1af56fa2b9f

adxl372_example: Fix count size to short
The FIFO buffer can contain up to 512 samples so the count parameter must
be 16-bit to be able to count to that value.

Fixes: c038c16c8e0c ("Arduino shields examples")
Signed-off-by: Mircea Caprioru <mircea.caprioru@analog.com>

Arduino Uno R3/examples/ADXL372_example/adxl372.cpp
@@ -42,7 +42,7 @@
 //#define ADXL_DEBUG
 
 static int adxl_read_reg_multiple(adxl_spi_handle *spi, unsigned char reg,
-                                  unsigned char count, unsigned char *val)
+                                  unsigned short count, unsigned char *val)
 {
     reg = reg << 1 | ADXL_SPI_RNW;
     return spi_write_then_read(spi, &reg, 1, val, count);   